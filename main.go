af63147ff0bb0a445b6d215b06c979d8ec3f357c
Josue Kouka <josuebrunel@gmail.com>
2020-12-13 02:07:59 +0100

    add EchoWB Client

---
+++ main.go
@@ -0,0 +1,104 @@
+package echowb
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/http/httptest"
+)
+
+// Headers is a type representing HTTP Headers
+type Headers map[string]string
+
+// URL is a type representing an URL with :
+// * Path
+// * Params
+// * Values
+type URL struct {
+	Path   string
+	Params []string
+	Values []string
+}
+
+// JSONObject holds the result of JSONDecode
+type JSONObject map[string]interface{}
+
+// JSONDecode returns an interface from a json formatted
+// http.ResponseRecorder.Body
+func JSONDecode(b *bytes.Buffer) (v JSONObject) {
+	v = JSONObject{}
+	err := json.Unmarshal(b.Bytes(), &v)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+// JSONEncode transforms interface into a []byte
+func JSONEncode(s interface{}) []byte {
+	out, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+// Client represents the client instance
+type Client struct {
+	E *echo.Echo
+}
+
+// New returns a client instance
+func New() (c Client) {
+	c = Client{echo.New()}
+	return
+}
+
+// Request is the method performing the request
+func (c Client) Request(method string, url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	methods := map[string]string{
+		"get":    http.MethodGet,
+		"post":   http.MethodPost,
+		"put":    http.MethodPut,
+		"delete": http.MethodDelete,
+		"patch":  http.MethodPatch,
+	}
+	req := httptest.NewRequest(methods[method], url.Path, bytes.NewReader(data))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	rec := httptest.NewRecorder()
+	ctx := c.E.NewContext(req, rec)
+	ctx.SetPath(url.Path)
+	ctx.SetParamNames(url.Params...)
+	ctx.SetParamValues(url.Values...)
+	handler(ctx)
+	return rec
+}
+
+// Get represents a Get Request
+func (c Client) Get(url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	return c.Request("get", url, handler, data, headers)
+}
+
+// Post represents a Post Request
+func (c Client) Post(url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	return c.Request("post", url, handler, data, headers)
+}
+
+// Put represents a Put Request
+func (c Client) Put(url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	return c.Request("put", url, handler, data, headers)
+}
+
+// Patch represents a Patch Request
+func (c Client) Patch(url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	return c.Request("patch", url, handler, data, headers)
+}
+
+// Delete represents a Delete Request
+func (c Client) Delete(url URL, handler echo.HandlerFunc, data []byte, headers Headers) *httptest.ResponseRecorder {
+	return c.Request("delete", url, handler, data, headers)
+}
